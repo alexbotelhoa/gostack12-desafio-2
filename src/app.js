47cc146c3f2c7ae557a1a8a5927faad07a7c208b
Alex Botelho <alexbotelho1@hotmail.com>
2020-06-16 13:54:53 -0300

    Project CRUD, Likes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 const repositories = [];
 
@@ -16,18 +15,54 @@ app.get("/repositories", (request, response) => {
 
 app.post("/repositories", (request, response) => {
   const { title, url, techs } = request.body;
+
+  const repositorie = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repositorie);
+  
+  return response.status(201).json(repositorie);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositorie = repositories.find(repositorie => repositorie.id === id);
+  if (!repositorie) return response.status(400).json({ error: 'Repositorie not found' });
+
+  repositorie.title = title;
+  repositorie.url = url;
+  repositorie.techs = techs;
+  
+  return response.status(200).json(repositorie)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+  if (repositorieIndex < 0) return response.status(400).json({ error: 'Repositorie not found'});
+
+  repositories.splice(repositorieIndex, 1)
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositorie = repositories.find(repositorie => repositorie.id === id);
+  if (!repositorie) return response.status(400).json({ error: 'Repositorie not found'});
+
+  repositorie.likes++;
+
+  return response.status(200).json(repositorie) 
 });
 
 module.exports = app;
